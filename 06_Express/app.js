199445d037ad054eaaaa0f1f3bd0eb3ba62ea7d0
Markus Chiarot <cmarkus@hotmail.ch>
2021-08-17 17:44:28 +0200

    create express project

---
+++ 06_Express/app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const mustache = require('mustache-express');
+const fs = require('fs').promises;
+fs.exists = require('fs').existsSync;
+const app = express();
+const port = process.env.port || 3000;
+const storage = __dirname + '/data.json';
+
+app.engine('mustache', mustache());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/pages');
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+async function getData() {
+  // create file if it does not exist
+  if (!fs.exists(storage)) {
+    await fs.writeFile(storage, '');
+    return [];
+  }
+
+  // read and return data
+  const data = await fs.readFile(storage, 'utf-8');
+  return data === '' ? [] : JSON.parse(data);
+}
+
+function writeData(data) {
+  return fs.writeFile(storage, JSON.stringify(data));
+}
+
+app.get('/', async (_req, res) => {
+  const todos = await getData();
+  res.render('index', {todos});
+});
+
+app.post('/add', async (req, res) => {
+  // get and check item
+  const {todo} = req.body;
+  if (!todo) return res.sendStatus(400);
+  
+  // get existing data and append
+  const todos = await getData();
+  todos.push(todo);
+
+  // persist data and redirect back to index
+  await writeData(todos);
+  res.redirect('/');
+});
+
+app.listen(port, () => console.log(`Started on http://localhost:${port}`));
