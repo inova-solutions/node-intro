2f8393bf6445dc002a5655b7ea6703e4c5e8f15f
Markus Chiarot <cmarkus@hotmail.ch>
2021-09-14 20:05:37 +0200

    document lib

--- 06_Project/lib/storage.js
+++ 06_Project/lib/storage.js
@@ -1,17 +1,34 @@
-import {promises as fs, constants} from 'fs';
+import { promises as fs, constants } from 'fs';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 
-const STORAGE_LOCATION = 'data.json';
+// put data.json at the correct location
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const STORAGE_LOCATION = resolve(__dirname, '../data.json');
 
-export const canAccessStorage = function() {
+/**
+ * Checks if storage can be accessed
+ * @returns {Promise<boolean>}
+ */
+export const canAccessStorage = () => {
   return fs.access(STORAGE_LOCATION, constants.W_OK).then(_ => true).catch(_ => false);
 }
 
-export const readData = async function() {
+/**
+ * Reads data or defaults to null if data is unavailable
+ * @returns {Promise<any | null>}
+ */
+export const readData = async () => {
   return await canAccessStorage()
     ? fs.readFile(STORAGE_LOCATION, 'utf-8').then(data => data !== '' ? JSON.parse(data) : null)
     : null;
 }
 
-export const writeData = function(data) {
+/**
+ * Writes data
+ * @param {any} data 
+ * @returns {Promise<void>}
+ */
+export const writeData = (data) => {
   return fs.writeFile(STORAGE_LOCATION, JSON.stringify(data));
 }
--- 06_Project/lib/todo.js
+++ 06_Project/lib/todo.js
@@ -1,16 +1,29 @@
 import { readData, writeData } from "./storage.js"
 
-export const getTodos = async function() {
+/**
+ * Gets all todos
+ * @returns {Promise<Array<string>>}
+ */
+export const getTodos = async () => {
   return (await readData()) ?? [];
 }
 
-export const appendTodo =  async function(todo) {
+/**
+ * Appends a todo
+ * @param {string} todo 
+ * @returns {Promise<Array<string>>}
+ */
+export const appendTodo =  async (todo) => {
   const todos = await getTodos();
   todos.push(todo);
   await writeData(todos);
   return todos;
 }
 
-export const clearTodos = function() {
+/**
+ * Clears all todos from storage
+ * @returns {Promise<void>}
+ */
+export const clearTodos = () => {
   return writeData([]);
 }
