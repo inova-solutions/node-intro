5b6d40bb4685f1421b904198d30c4d2f486118e3
Markus Chiarot <cmarkus@hotmail.ch>
2021-09-14 20:31:34 +0200

    make task for frontend

--- 06_Project/frontend/src/app/todo.service.ts
+++ 06_Project/frontend/src/app/todo.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
-import { delay, map } from "rxjs/operators";
 
 @Injectable({ providedIn: 'root' })
 export class TodoService {
@@ -9,11 +8,21 @@ export class TodoService {
   constructor(private http: HttpClient) { }
 
   public getTodos() {
-    return this.http.get<string[]>(this.api).pipe(map(todos => todos.reverse()));
+    /**
+     * TASK:
+     * reverse the list of todos before returning it
+     * hint: use RxJS map operator
+     */
+    return this.http.get<string[]>(this.api);
   }
 
   public addTodo(todo: string) {
-    return this.http.post(this.api, { todo }, { responseType: 'text' }).pipe(delay(1000));
+    /**
+     * TASK:
+     * give this observable 1 second delay to simulate a heavy API call
+     * hint: use RxJS delay operator
+     */
+    return this.http.post(this.api, { todo }, { responseType: 'text' });
   }
 
   public clearTodos() {
